906895c0bb13089efba5b165b1feb4af92830537
soravpatel10 <soravpatel10@gmail.com>
2025-11-25 19:12:15 +0530

    Create User.java

---
+++ User.java
@@ -0,0 +1,16 @@
+package model;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+}
